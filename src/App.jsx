e86fa6cb48af9c3539c8201b13a01d9f7a149c75
ludo loops <ludoloops@protonmail.com>
2021-12-01 18:10:27 +0100

    Feat: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import styled from 'styled-components'
-
 import { Navbar, Sidebar, Footer } from './components'
+
 import {
   Home,
   SingleProduct,
