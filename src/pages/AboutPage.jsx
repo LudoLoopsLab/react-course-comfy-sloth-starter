8700193c5f49e93dfc8f3a45cdfccb4edad8a4fe
ludo loops <ludoloops@protonmail.com>
2021-12-03 00:04:26 +0100

    Feat: about, checkout, page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline' />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+            expedita vitae doloremque explicabo officia exercitationem minima
+            dolorum illum sequi, aspernatur esse laboriosam? At minus distinctio
+            accusantium minima, libero vero nisi?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
