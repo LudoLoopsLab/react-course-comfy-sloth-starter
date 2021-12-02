d5b44ba0c082a66f8b1c4d77e947bff3d7789039
ludo loops <ludoloops@protonmail.com>
2021-12-03 00:32:30 +0100

    feat: home page services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,36 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>
+            custom furniture <br />
+            built only for you
+          </h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            cupiditate molestias, inventore eius provident atque nesciunt a
+            commodi deleniti? Reprehenderit obcaecati repellat officiis
+            repudiandae, ipsa nam consequuntur dolor delectus voluptatum.
+          </p>
+        </article>
+        <div className='services-center'>
+          {services.map(service => {
+            const { id, icon, title, text } = service
+            return (
+              <article key={id} className='service'>
+                <span className='icon'> {icon} </span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
