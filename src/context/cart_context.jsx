d79643bf29f716bd508f40f36ad26c64f01c0686
ludo loops <ludoloops@protonmail.com>
2021-12-06 19:14:46 +0100

    Feat: add to cart reducer - existing item

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -21,6 +21,7 @@ export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const addToCart = (id, color, amount, product) => {
+    console.log(color)
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
   }
 
--- src/redux/cart_reducer.js
+++ src/redux/cart_reducer.js
@@ -8,9 +8,23 @@ import {
 
 const cart_reducer = (state, action) => {
   if (action.type === ADD_TO_CART) {
+    console.log('ADD_TO_CART')
     const { id, color, amount, product } = action.payload
     const tempItem = state.cart.find(i => i.id === id + color)
+    console.log('tempItem', tempItem)
     if (tempItem) {
+      const tempCart = state.cart.map(cartItem => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max
+          }
+          return { ...cartItem, amount: newAmount }
+        } else {
+          return cartItem
+        }
+      })
+      return { ...state, cart: tempCart }
     } else {
       const newItem = {
         id: id + color,
@@ -21,7 +35,7 @@ const cart_reducer = (state, action) => {
         price: product.price,
         max: product.stock,
       }
-      return { ...state, cart: [...state.cart], newItem }
+      return { ...state, cart: [...state.cart, newItem] }
     }
   }
   return state
